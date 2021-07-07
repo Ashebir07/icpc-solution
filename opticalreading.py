5ce2e29454342c5a7a5e80b07d0b45efd2e0bd97
Ashebir07 <83369330+Ashebir07@users.noreply.github.com>
2021-07-07 14:53:43 +0300

    Add files via upload

---
+++ opticalreading.py
@@ -0,0 +1,29 @@
+while True:
+    
+    try:
+        tc=int(input())
+        if tc==0:
+            break
+        else:
+            mls=[]
+            
+            for i in range(tc):
+                inx=0
+                k=1
+                a=["A","B","C","D","E"]
+                y=[int(i) for i in input().split()]
+                for i in range(len(y)):
+                    if y[i]<=127:
+                        inx=i
+                        k+=1
+
+                        
+                if k>2 or k==1:
+                    mls.append("*")
+                else:
+                    mls.append(a[inx])
+            for i in mls:
+                print(i)
+    except:
+        break
+        
